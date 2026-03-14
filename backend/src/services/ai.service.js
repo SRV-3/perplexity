c12d28effcfa18b0e21297ba9c0a74f8ee87c88b
Sourav <thakursourav778@gmail.com>
2026-03-14 20:16:05 +0530

    daily commit

---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash-lite",
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+export async function testAi() {
+  model.invoke("what is ai explain under 100 words").then((response) => {
+    console.log(response.text);
+  });
+}
