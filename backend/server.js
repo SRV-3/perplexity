28e6662709a921bd9ea3d6ae6bf8807a4c6a0f45
Sourav <thakursourav778@gmail.com>
2026-03-18 21:37:01 +0530

    implement socket.io integration for real-time chat functionality

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import "dotenv/config";
 import app from "./src/app.js";
+import http from "http";
 import connectDB from "./src/config/databse.js";
+import { initSocket } from "./src/sockets/server.socket.js";
 // import { testAi } from "./src/services/ai.service.js";
 
 const PORT = process.env.PORT || 8000;
 
+const httpServer = http.createServer(app);
+
+initSocket(httpServer);
+
 connectDB();
 // testAi();
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
